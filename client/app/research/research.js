a827dbfd9559deb3e47f6267ae8bc91351890752
Paul Quenault <plquenault@gmail.com>
2018-05-16 22:17:38 +0200

    Finalize research behaviour

--- client/app/research/research.js
+++ client/app/research/research.js
@@ -8,7 +8,7 @@
   function Research($scope, DAO) {
     var vm = this;
 
-    vm.contentType = 'list';
+    vm.contentState = 'all';
     vm.research = '';
     vm.category = 'Écoles';
 
@@ -25,32 +25,39 @@
       vm.category = 'Écoles';
     };
 
-    function obj(obj, id) {
-      return obj._id === id;
-    }
-
     vm.school = {};
     vm.showSchool = function(schoolId) {
-      vm.contentType = 'element';
+      // Change content view state
+      vm.contentState = 'one';
+      // Find the school
       vm.school = vm.schools.find(school => school._id === schoolId);
+      // Find the courses
+      vm.course = vm.courses.filter(course => course.school.includes(schoolId));
+      // Map animation
+      vm.map.flyTo(vm.school.location.coordinates, 12);
     };
 
-    vm.course = {};
+    vm.course = [];
     vm.showCourse = function(courseId) {
-      vm.contentType = 'element';
-      vm.course = vm.courses.find(course => course._id === courseId);
+      // Change content view state
+      vm.contentState = 'one';
+      // Find the course
+      vm.course = [vm.courses.find(course => course._id === courseId)];
+      // Find the school
+      vm.school = vm.schools.find(school => school._id === vm.course[0].school);
+      // Map animation
+      vm.map.flyTo(vm.school.location.coordinates, 12);
     };
 
     // Map base settings
-    var map = L.map('mapid');
-    map.setView([46.61, 2.72]);
-    map.setZoom(6);
+    vm.map = L.map('mapid');
+    vm.map.setView([46.61, 2.72], 6);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
   		maxZoom: 18,
   		id: 'mapbox.streets',
   		accessToken: 'pk.eyJ1IjoicHBlcmdldCIsImEiOiJjamZibjlibzcyNXgyMnhucjFxemc5YWQxIn0.ZTNERHrUn5YkoD6sUEJx_Q'
-  	}).addTo(map);
+  	}).addTo(vm.map);
 
     // Get schools
     vm.schools = [];
@@ -67,36 +74,32 @@
         shadowAnchor: [16, 32],
         popupAnchor: [0, -32]
       });
+
+      // Map clusters
       var markers = L.markerClusterGroup({
         maxClusterRadius: 30
       });
+
+      // Set markers coordinates, icon, bind popup and click event
       for (var i = 0; i < vm.schools.length; i++) {
         markers
           .addLayer(
             L.marker(vm.schools[i].location.coordinates, {icon: schoolIcon})
               .bindPopup(vm.schools[i].name)
               .on('click', function(e) {
-                var mc = e.target.getLatLng();
-                for (var i = 0; i < vm.schools.length; i++) {
-                  var sc = vm.schools[i].location.coordinates;
-                  if (mc.lat === sc[0] && mc.lng === sc[1]) {
-                    DAO.getSchoolCourses(vm.schools[i]._id).then(function(response) {
-                      var research = '';
-                      for (var key in response.data) {
-                        research += response.data[key].name;
-                      }
-                      vm.research = research;
-                      // $scope.$digest();
-                    }, function(response) {
-                      console.log('Error during getSchoolCourses');
-                    });
-                    break;
-                  }
-                }
+                // Retrieve marker coordinates
+                var markerCoordinates = e.target.getLatLng();
+                // Find the school id by coordinates
+                var schoolId = vm.schools.find(school => school.location.coordinates[0] === markerCoordinates.lat && school.location.coordinates[1] === markerCoordinates.lng)._id;
+                // Show the school
+                vm.showSchool(schoolId);
+                // Tell the scope to watch for changes now, otherwise it won't be aware of changes
+                $scope.$digest();
               })
           );
       }
-      map.addLayer(markers);
+      // Add the markers to the map
+      vm.map.addLayer(markers);
     }, function(response) {
       console.log('Error during getSchools');
     });
@@ -107,6 +110,14 @@
       vm.courses = response.data;
     }, function(response) {
       console.log('Error during getCourses');
+    });
+
+    // Get former students
+    vm.formerStudents = [];
+    DAO.getFormerStudents().then(function(response) {
+      vm.formerStudents = response.data;
+    }, function(response) {
+      console.log('Error during getformerStudents');
     });
   }
 
